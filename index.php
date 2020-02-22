018c408d941b72c56dd43d88a1bb23a3ac2bcd7c
k1193 <61071131+k1193@users.noreply.github.com>
2020-02-22 18:17:28 -0500

    ejercici2

--- index.php
+++ index.php
@@ -22,6 +22,52 @@ $division =$variable1 / $variable2;
 
 
   echo("preguna#2 <br>");
+   
+  $peso =85;
+  $altura= 178;
+  $imc = $peso /($altura*$altura);
+
+     if ($imc <= 18,4)
+          echo(" Insuficiencia de peso  <br>");
+
+         else {
+
+              if($imc <= 18,5 && $imc <=24,9)
+
+                 echo("peso normal <br> ");}
+
+                 else {
+
+                    if($imc<=25 && $imc<=29,9)
+               
+                      echo("Sobrepeso <br>");}
+
+                      else{
+
+                           if($imc<=30&&$imc<=34,9)
+
+                              echo("Obecidad1 <br>");}
+
+                              else{
+
+                                   if($imc<=35&& $imc<= 39,9)
+
+                                      echo("Obesidad2 <br>");}
+                                      
+                                      else{
+                                           $imc>=40
+                                             echo("esta muy gordito <br>");}
+                                      
+
+                              
+                     
+         
+         
+               
+                
+                
+                     
+
 
   
 
