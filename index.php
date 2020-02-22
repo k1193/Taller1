0dcbb4db939ab016df5c0f4c58b9eba0a2d30878
k1193 <61071131+k1193@users.noreply.github.com>
2020-02-21 21:11:52 -0500

    pregunta1

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
 <?php
 
 
-echo("hola");
+echo("taller 1  generalidaes  presentado por ksandra <br>");
+echo("preguna#1 <br>");
+
+$variable1=  400;
+$variable2= 80;
+
+
+$suma=$variable1 + $variable2;
+$resta =$variable1 - $variable2;
+$multiplicacion =$variable1 * $variable2;
+$division =$variable1 / $variable2;
+
+
+
+  echo ("la suma de:".$variable1."+".$variable2."=".$suma."<br>");
+  echo ("la resta de:".$variable1."-".$variable2."=".$resta."<br>");
+  echo ("la summultiplicacion de:".$variable1."*".$variable2."=".$multiplicacion."<br>");
+  echo ("la division de:".$variable1."/".$variable2."=".$division."<br>");
+
+
+  echo("preguna#2 <br>");
+
+
+
+
 
 
 
